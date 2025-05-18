00864ad587c610ec7b1538cbb6a891d8f3ed31c9
saccles <silasaccles@gmail.com>
2025-05-18 19:53:53 -0400

    Create blueprint for script (username_enumeration_via_response_timing)

---
+++ topics/authentication/username_enumeration_via_response_timing/script.py
@@ -0,0 +1,23 @@
+#
+
+import requests
+
+
+def get_valid_user_response_time():
+    pass
+
+
+def enumerate_user_response_timing():
+    pass
+
+
+def bruteforce_password():
+    pass
+
+
+def main():
+    valid_user_response_time = get_valid_user_response_time()
+
+
+if __name__ == "__main__":
+    main()
